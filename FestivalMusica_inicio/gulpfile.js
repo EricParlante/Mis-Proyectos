af1dce2c10b57e0df1b1508b7b7c5c34535a3400
ericparlante <erickparlante@gmail.com>
2022-10-30 21:31:07 -0300

    Convirtiendo imagenes

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,6 +1,16 @@
-const {src, dest, watch } = require("gulp");
+
+const {src, dest, watch, parallel } = require("gulp");
+//Parallel permite ejecutar dos funciones a la vez.
+
+//DEPENDENCIAS PARA CSS
 const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber')
+const plumber = require('gulp-plumber');
+
+
+// DEPENDENCIAS PARA IMAGENES
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done){
     src('src/scss/**/*.scss') //Identificar el archivo de SASS
@@ -11,10 +21,33 @@ function css(done){
     done(); //Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3 
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp(done){
+    const opciones = { //Variable con la calidad que deseamos obtener
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}') //Identif. el formato q deseamos transformar
+    .pipe(webp(opciones)) //llamamos la funcion que convierte las imagenes
+    .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css)
 
     done();
 }
+
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev); //permite ejecutar dos funciones
